fc301f267d74ab215e28e2957f4ec312337e3f9f
William Liu <orangetnt827@gmail.com>
2025-01-21 19:22:45 -0600

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,15 @@
 
 <div class="min-h-screen bg-sky-200 font-sans text-black">
   {@render children()}
+  <footer class="mt-4 text-center">
+    <p>
+      <a
+        class="text-sky-600 underline"
+        href="https://github.com/renaissancedog/weather"
+        target="_blank">Star or fork</a
+      >
+      this repo.
+    </p>
+    <p class="mb-4 mt-2">Made with ❤️ by William, 2025</p>
+  </footer>
 </div>
